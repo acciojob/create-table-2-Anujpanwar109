b26b0b306ffccace3df320c0929baf3d5e90db06
Anujpanwar109 <166197689+Anujpanwar109@users.noreply.github.com>
2025-02-18 13:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+let table=document.querySelector("#myTable");
 function createTable() {
     //Write your code here
+	const rows=parseInt(prompt("Input number of rows"),10);
+	const cols=parseInt(prompt("Input number of columns"),10);
+
+	if(isNaN(rows)||isNaN(cols)||rows<=0||cols<=0){
+		alert("Enter a valid no. of rows and cols")
+		return;
+	}
+
+	 table.innerHTML = '';
+	
+	for(let i=0;i<rows;i++){
+		let row=document.createElement("tr");
+		for(let j=0;j<cols;j++){
+			let cell=document.createElement("td");
+			cell.innerText=`Row-${i} columns-${j}`
+			row.appendChild(cell)
+			
+		}
+		table.appendChild(row)
+	}
+
+	
+	
   
 }
